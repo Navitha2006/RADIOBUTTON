8fd01188977785e458dbad1f36ceef4904cce87f
Navitha230701208 <230701208@rajalakshmi.edu.in>
2026-03-18 12:46:07 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.radiobuttondemo
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rbRed -> mainLayout.setBackgroundColor(Color.RED)
+                R.id.rbGreen -> mainLayout.setBackgroundColor(Color.GREEN)
+                R.id.rbBlue -> mainLayout.setBackgroundColor(Color.BLUE)
+            }
+        }
+    }
+}
